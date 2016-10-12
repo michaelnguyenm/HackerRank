71ae2ac203f20a3ce8a7826ef7801981727a7f3b
Michael Nguyen <michaelmnguyen@utexas.edu>
2016-10-11 17:03:03 -0700

    Create ArraysDS.java

---
+++ Data-Structures/Arrays/ArraysDS.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        for(int i = n-1; i >= 0; --i){
+            System.out.printf("%d ", arr[i]);
+        }
+    }
+}
